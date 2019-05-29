78054d3f76a357979710d24f52d5fae603245e44
saurabhmishraepam <mishra.saurabh19@gmail.com>
2019-05-29 13:48:09 +0530

    api key

--- android-app/Araksa/app/src/main/java/com/gdo/araksa/araksa/constants/GoogleToken.java
+++ android-app/Araksa/app/src/main/java/com/gdo/araksa/araksa/constants/GoogleToken.java
@@ -2,5 +2,5 @@ package com.gdo.araksa.araksa.constants;
 
 public class GoogleToken {
 
-    public static String apitoken="AIzaSyBsAhXRBCTOu_lY7r8Z4NQ7RwW2HYAU4LA";
+    public static String apitoken="";
 }
